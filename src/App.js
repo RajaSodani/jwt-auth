4ff4ffe70404f3bebdb346eb7d468432c5a15db3
Raja Sodani <raja.sodani@gmail.com>
2024-06-30 01:51:37 +0530

    final frontend part

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Login from './components/Login';
+import AdminDashboard from './components/AdminDashboard';
+import EditorDashboard from './components/EditorDashboard';
+import ViewerDashboard from './components/ViewerDashboard';
+import Signup from './components/Signup';
+import { setAuthToken } from './api/api';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [token, setToken] = useState(localStorage.getItem('token'));
+
+    useEffect(() => {
+        setAuthToken(token);
+    }, [token]);
+
+    const isAuthenticated = () => !!token; // Check if the token exists
+
+    return (
+        <Router>
+            <Routes>
+                <Route 
+                    path="/signup" element={<Signup/>} />
+                <Route 
+                    path="/login" element={<Login setToken={setToken} />} />
+                <Route 
+                    path="/admin" element={isAuthenticated() ? <AdminDashboard /> : <Navigate to="/login" />} />
+                <Route 
+                    path="/editor" element={isAuthenticated() ? <EditorDashboard /> : <Navigate to="/login" />} />
+                <Route 
+                    path="/viewer"element={isAuthenticated() ? <ViewerDashboard /> : <Navigate to="/login" />} />
+                <Route 
+                    path="*" element={<Navigate to="/login" />} />
+            </Routes>
+        </Router>
+    );
+};
 
 export default App;
